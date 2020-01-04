9364d8f41620a52ec44f7e2c7afac38e2e8e1f7b
Marc Littlemore <marc.littlemore@gmail.com>
2020-01-04 12:21:14 +0000

    Fix dodgy semicolon

--- src/components/audioCollection.js
+++ src/components/audioCollection.js
@@ -8,17 +8,21 @@ const getAllNodes = data => {
   return edges.map(edge => edge.node);
 };
 
+const AudioItem = ({ data }) => {
+  const {
+    frontmatter: { title },
+  } = data;
+  return <h3>{title}</h3>;
+};
+
 const AudioCollection = ({ title, data }) => {
   const nodes = getAllNodes(data);
   return (
     <div className="mv3 mw8 center">
       <h2 className="ttu f5 f4-m f3-ns mt3 mb0 tc">{title}</h2>
-      {nodes.map(node => {
-        const {
-          frontmatter: { title },
-        } = node;
-        return <h3>{title}</h3>;
-      })}
+      {nodes.map(node => (
+        <AudioItem data={node} />
+      ))}
     </div>
   );
 };

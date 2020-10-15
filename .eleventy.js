925528ac019eb3dcd8b960c5f65e2809391238c0
Marc Littlemore <marc.littlemore@gmail.com>
2020-10-15 21:08:25 +0100

    Tidy up config and move audio layout

--- .eleventy.js
+++ .eleventy.js
@@ -13,36 +13,6 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
   eleventyConfig.addPassthroughCopy({ 'src/favicons': 'favicons' });
   eleventyConfig.addPassthroughCopy({ 'src/amplitude.css': 'amplitude.css' });
-  // eleventyConfig.addPassthroughCopy({'src/thumbnails': 'thumbnails'});
-  // eleventyConfig.addPassthroughCopy({'src/favicons': 'favicons'});
-  // eleventyConfig.addPassthroughCopy({'src/robots.txt': 'robots.txt'});
-  // eleventyConfig.addPassthroughCopy({'src/browserconfig.xml': 'browserconfig.xml'});
-  // eleventyConfig.addPassthroughCopy({'src/distjs/': 'js/'});
-
-  // Temporary aliases
-  eleventyConfig.addLayoutAlias('audio', 'layouts/audio.html');
-  //   eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-  //   eleventyConfig.addLayoutAlias('homepage', 'layouts/homepage.html');
-  //   eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-  //   eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-  //   eleventyConfig.addLayoutAlias('articles', 'layouts/articles.html');
-  //   eleventyConfig.addLayoutAlias('biography', 'layouts/biography.html');
-  //   eleventyConfig.addLayoutAlias(
-  //     'landingpage-default',
-  //     'layouts/landingpage-default.html'
-  //   );
-  //   eleventyConfig.addLayoutAlias(
-  //     'landingpage-long',
-  //     'layouts/landingpage-long.html'
-  //   );
-  //   eleventyConfig.addLayoutAlias(
-  //     'landingpage-thank-you',
-  //     'layouts/landingpage-thank-you.html'
-  //   );
-
-  //   eleventyConfig.addLiquidFilter('markdownify', (value) => {
-  //     return markdownLib.render(value);
-  //   });
 
   eleventyConfig.addShortcode('version', function () {
     return String(Date.now());
@@ -100,12 +70,6 @@ module.exports = (eleventyConfig) => {
       )
     );
   });
-
-  // Liquid template options
-  //   eleventyConfig.setLiquidOptions({
-  //     dynamicPartials: false,
-  //     root: ['src/_includes/_includes', 'src/_includes/_includes/social', '.'],
-  //   });
 
   eleventyConfig.setDataDeepMerge(true);
 
